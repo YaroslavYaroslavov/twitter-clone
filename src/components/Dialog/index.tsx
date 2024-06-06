43a4930ceea375eb7cd0c0686ed1c6debd9bdb6e
unknown <tvoibaget@mail.ru>
2024-06-07 02:39:57 +0300

    wtf

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,35 +1,44 @@
+import { Modal } from 'components/Modal';
 import { sendMessage } from 'components/SendMessage';
-import { onValue,ref } from 'firebase/database';
+import { onValue, ref } from 'firebase/database';
 import { db } from 'firebaseConfig/firebase';
 import { StateInterface } from 'interface';
-import React, { useCallback,useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import {
-  Avatar, 
-  AvatarLink, 
-  ButtonContainer, 
-  CloseButton, 
+  Avatar,
+  AvatarLink,
+  ButtonContainer,
+  CloseButton,
   DialogContainer,
-  InputContainer, 
-  MessageContent, 
-  MessageInput, 
-  MessageItem, 
-  MessagesContainer, 
-  SendButton, 
+  InputContainer,
+  MessageContent,
+  MessageInput,
+  MessageItem,
+  MessagesContainer,
+  SendButton,
   TheirMessageInfo,
-  Username} from './styled';
- 
+  Username,
+} from './styled';
+
 const Dialog = ({ conversation, onClose }) => {
   const [messages, setMessages] = useState([]);
   const [newMessageText, setNewMessageText] = useState('');
   const currentUserInfo = useSelector((state: StateInterface) => state.userInfo);
-  const users = useSelector((state: StateInterface) => state.users)
+  const users = useSelector((state: StateInterface) => state.users);
   const [interlocutorInfo, setInterlocutorInfo] = useState(null);
-  const messagesEndRef = useRef(null); 
+  const messagesEndRef = useRef(null);
+  const [showParticipantsModal, setShowParticipantsModal] = useState(false);
+  const [participants, setParticipants] = useState([]);
 
-  // Функция скроллинга
+  const handleParticipantsClick = () => {
+    setShowParticipantsModal(true);
+    console.log('123');
+  };
+
+
   const scrollToBottom = useCallback(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: 'instant' });
@@ -38,89 +47,137 @@ const Dialog = ({ conversation, onClose }) => {
 
   useEffect(() => {
     if (currentUserInfo && conversation) {
-      setMessages([])
-      const messagesRef = conversation.name ? ref(db, `message/usersWithMessage/${currentUserInfo.userId}/chats/${conversation.id}/messages`): ref(db, `message/usersWithMessage/${currentUserInfo.userId}/users/${conversation.id}`) 
-        onValue(messagesRef, (snapshot) => {
-          
-          const messagesData = snapshot.val()
-          
-          if (messagesData) {
-            const messagesList = Object.keys(messagesData).map((messageId) => ({
-              id: messageId,
-              text: messagesData[messageId].text,
-              sender: messagesData[messageId].sender,
-              timestamp: messagesData[messageId].timestamp,
-            }));
-            setMessages(messagesList);
-          } 
-  
-        });
+      setMessages([]);
+      const messagesRef = conversation.name
+        ? ref(
+            db,
+            `message/usersWithMessage/${currentUserInfo.userId}/chats/${conversation.id}/messages`
+          )
+        : ref(db, `message/usersWithMessage/${currentUserInfo.userId}/users/${conversation.id}`);
+      onValue(messagesRef, (snapshot) => {
+        const messagesData = snapshot.val();
+        if (messagesData) {
+          const messagesList = Object.keys(messagesData).map((messageId) => ({
+            id: messageId,
+            text: messagesData[messageId].text,
+            sender: messagesData[messageId].sender,
+            timestamp: messagesData[messageId].timestamp,
+          }));
+          setMessages(messagesList);
+        }
+      });
+
+      const interlocutorRef = ref(db, `users/${conversation.id}`);
+      onValue(interlocutorRef, (snapshot) => {
+        const interlocutorData = snapshot.val();
+        setInterlocutorInfo(interlocutorData);
+      });
+    }
+
+    const participantsRef = ref(
+      db,
+      `message/usersWithMessage/${currentUserInfo.userId}/chats/${conversation.id}/users`
+    );
+    onValue(participantsRef, (snapshot) => {
+      const participantsData = snapshot.val();
+      if (participantsData) {
+        const participantsIds = Object.keys(participantsData);
+        const participantsList = participantsIds.map((userId) => participantsData[userId]);
+      
+
+        const participantsWithInfo = participantsList.map((participant) => users.find((user) => user.userId === participant));
+        // console.log(participantsWithInfo)
     
-        const interlocutorRef = ref(db, `users/${conversation.id}`);
-        onValue(interlocutorRef, (snapshot) => {
-          const interlocutorData = snapshot.val();
-          setInterlocutorInfo(interlocutorData);
-        });
+
+        setParticipants(participantsWithInfo)
       }
-     
-  }, [conversation]);
+      
+    });
+  }, [conversation, currentUserInfo, users]);
 
   useEffect(() => {
-    scrollToBottom(); // Прокрутка до последнего сообщения 
+    scrollToBottom();
   }, [messages, scrollToBottom]);
 
   const handleSendMessage = () => {
     if (newMessageText.trim() === '') return;
-   
     sendMessage(newMessageText, conversation.id, currentUserInfo.userId, !conversation.name);
-
     setNewMessageText('');
-  };  
-
-
+  };
   return (
     <DialogContainer>
-      {interlocutorInfo?.username ? <h2>Чат с {interlocutorInfo?.username}</h2> :
-       conversation?.name ?  <h2>Беседа {conversation?.name}</h2> : 
-       <h2>Загрузка...</h2>
-      }
+      {interlocutorInfo?.username ? (
+        <h2>Чат с {interlocutorInfo?.username}</h2>
+      ) : conversation?.name ? (
+        <h2>
+          {conversation?.name}
+          <ParticipantsCount onClick={handleParticipantsClick}>
+            {Object.keys(participants).length} участника
+          </ParticipantsCount>
+        </h2>
+      ) : (
+        <h2>Загрузка...</h2>
+      )}
 
       <MessagesContainer>
-      {messages.length ? messages.map((message) =>  {
-        const messageSender = users.find(user => user.userId === message.sender)
-      
-    return  (
-      <MessageItem key={messageSender.id} className={messageSender.userId === currentUserInfo.userId ? 'mine' : 'theirs'}>
-      {messageSender.userId !== currentUserInfo.userId && (
-        <TheirMessageInfo>
-          <AvatarLink>
-            <Link to={`/profile/${messageSender.userlink}`}>
-              <Avatar src={messageSender.avatar} alt="Avatar" />
-            </Link>
-            <Username>{messageSender.username}</Username>
-          </AvatarLink>
-        </TheirMessageInfo>
-      )}
-      <MessageContent className={message.sender === currentUserInfo.userId ? 'mine' : 'theirs'}>
-        {message.text}
-      </MessageContent>
-    </MessageItem>
-    )
-       
-}) : 'пока пусто'}
-      <div ref={messagesEndRef} />
-    </MessagesContainer>
+        {messages.length
+          ? messages.map((message) => {
+              const messageSender = users.find((user) => user.userId === message.sender);
+              return (
+                <MessageItem
+                  key={messageSender.id}
+                  className={messageSender.userId === currentUserInfo.userId ? 'mine' : 'theirs'}
+                >
+                  {messageSender.userId !== currentUserInfo.userId && (
+                    <TheirMessageInfo>
+                      <AvatarLink>
+                        <Link to={`/profile/${messageSender.userlink}`}>
+                          <Avatar src={messageSender.avatar} alt="Avatar" />
+                        </Link>
+                        <Username>{messageSender.username}</Username>
+                      </AvatarLink>
+                    </TheirMessageInfo>
+                  )}
+                  <MessageContent
+                    className={message.sender === currentUserInfo.userId ? 'mine' : 'theirs'}
+                  >
+                    {message.text}
+                  </MessageContent>
+                </MessageItem>
+              );
+            })
+          : 'пока пусто'}
+        <div ref={messagesEndRef} />
+      </MessagesContainer>
       <InputContainer>
         <MessageInput
           value={newMessageText}
           onChange={(e) => setNewMessageText(e.target.value)}
-          placeholder="Type a message..."
+          placeholder="Напишите сообщение..."
         />
         <ButtonContainer>
           <SendButton onClick={handleSendMessage}>Отправить</SendButton>
           <CloseButton onClick={onClose}>Закрыть</CloseButton>
         </ButtonContainer>
       </InputContainer>
+      <Modal active={showParticipantsModal} setActive={setShowParticipantsModal}>
+        <div>
+          <h3>Участники беседы:</h3>
+          <ul>
+            {participants.map((participant) => (
+              <li key={participant.userId}>
+                <AvatarLink>
+                <Username>{participant.username}</Username>
+                  <Link to={`/profile/${participant.userlink}`}>
+                    <Avatar src={participant.avatar} alt="Avatar" />
+                  </Link>
+                 
+                </AvatarLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Modal>
     </DialogContainer>
   );
 };
